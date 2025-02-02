4bc8e813507bc4fbc9594003eafab6297e8e23f9
geek1060 <g24ai1060@iitj.ac.in>
2025-02-02 21:48:57 +0530

    Update SVM2.py

--- SVM2.py
+++ SVM2.py
@@ -32,4 +32,4 @@ def fetch_data():
         return jsonify({"error": "Data for this city is not present"}), 404
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.11', port=5000)
+    app.run(host='192.168.1.195', port=5000)
